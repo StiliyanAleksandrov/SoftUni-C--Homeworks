8282be71d7700efef0cb95046bc79095bc8c1ebc
Stiliyan Aleksandrov <71715129+StiliyanAleksandrov@users.noreply.github.com>
2020-09-29 15:26:51 +0300

    Add Activation Keys task

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace _13._Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "Generate")
+            {
+                string[] commands = input.Split(">>>");
+                string currentCommand = commands[0];
+
+                if (currentCommand == "Contains")
+                {
+                    string substring = commands[1];
+
+                    if (activationKey.Contains(substring))
+                    {
+                        Console.WriteLine($"{activationKey} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (currentCommand == "Flip")
+                {
+                    string upperOrLower = commands[1];
+                    int startIndex = int.Parse(commands[2]);
+                    int endIndex = int.Parse(commands[3]);
+                    int length = endIndex - startIndex;
+
+                    if (upperOrLower == "Upper")
+                    {
+                        string oldSubstring = activationKey.Substring(startIndex, length);
+                        string newSubstring = activationKey.Substring(startIndex, length).ToUpper();
+
+                        activationKey = activationKey.Replace(oldSubstring, newSubstring);
+                    }
+                    else
+                    {
+                        string oldSubstring = activationKey.Substring(startIndex, length);
+                        string newSubstring = activationKey.Substring(startIndex, length).ToLower();
+
+                        activationKey = activationKey.Replace(oldSubstring, newSubstring);
+                    }
+
+                    Console.WriteLine(activationKey);
+                }
+                else
+                {
+                    int startIndex = int.Parse(commands[1]);
+                    int endIndex = int.Parse(commands[2]);
+                    int length = endIndex - startIndex;
+
+                    activationKey = activationKey.Remove(startIndex, length);
+
+                    Console.WriteLine(activationKey);
+                }
+            }
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+    }
+}
